76632c299d90de8b1e320912f8634cc4a027d371
Adrian Ortiz <adortiz@csumb.edu>
2018-11-28 11:41:21 -0800

    Changed from flask application to pyqt5 GUI

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import sys
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton,
+                                QLineEdit, QHBoxLayout, QVBoxLayout, QComboBox)
+from PyQt5.QtCore import pyqtSlot
 
 
-app = Flask(__name__)
+class MyWindow(QWidget):
+    def __init__(self):
+        super().__init__()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+        hbox1 = QHBoxLayout()
+        self.label1 = QLabel('Enter url: ')
+        self.line_edit = QLineEdit()
+
+        hbox1.addWidget(self.label1)
+        hbox1.addWidget(self.line_edit)
+
+        self.button = QPushButton('Search', self)
+        hbox1.addWidget(self.button)
+
+        self.setLayout(hbox1)
+        self.title = 'Audio/Video Downloader'
+        self.left = 300
+        self.top = 300
+        self.width = 500
+        self.height = 300
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+app = QApplication(sys.argv)
+main = MyWindow()
+main.show()
+sys.exit(app.exec_())
