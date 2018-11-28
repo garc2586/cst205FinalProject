785f8e13415f0d83404d7288f80de026eb3ab857
garc2586 <31326232+garc2586@users.noreply.github.com>
2018-11-27 22:53:22 -0800

    added the python file that downloads audio

---
+++ download_yt_vid.py
@@ -0,0 +1,19 @@
+
+import pytube
+link = "https://www.youtube.com/watch?v=PTpgUmIWPis"
+yt = pytube.YouTube(link)
+print('Video')
+videos = yt.streams.all()
+for x in videos:
+	print(x)
+
+print('Audio')
+#to get only audio use
+audio = yt.streams.filter(only_audio=True).all()
+for x in audio:
+	print(x)
+audio_download = yt.streams.filter(only_audio=True).first()
+audio_download.download('C:\\Users\\cgarc\\Desktop\\cst205\\final_project\\yt_vids')
+
+#stream = yt.streams.first()
+#stream.download('C:\\Users\\cgarc\\Desktop\\cst205\\final_project\\yt_vids')
