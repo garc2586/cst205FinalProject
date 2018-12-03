9aa5a4f79961f10a2edc05971b9e99e15f17a9bb
garc2586 <31326232+garc2586@users.noreply.github.com>
2018-12-03 15:59:48 -0800

    tried to run downlader in a thread

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys, pytube
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton,
                                 QLineEdit, QHBoxLayout, QVBoxLayout, QComboBox)
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import pyqtSlot, QThread
 
 
 class MyWindow(QWidget):
     def __init__(self):
 
         super().__init__()
+
+        self.pytubeCallThread = pytubeCallThread()
 
         hbox1 = QHBoxLayout()
         self.label1 = QLabel('Enter url: ')
@@ -30,22 +33,44 @@ class MyWindow(QWidget):
         self.height = 300
         self.initUI()
 
+        #loading gif
+        self.status_txt = QLabel()
+        movie = QMovie("C:/Users/cgarc/Desktop/cst205/205git/cst205FinalProject/ajax-loader.gif")
+        self.status_txt.setMovie(movie)
+        movie.start()
+        self.status_txt.setHidden(True)
+        hbox1.layout().addWidget(self.status_txt)
+
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
 
     def getVideo(self):
-        self.yt = pytube.YouTube(self.yt_url.text())
+        self.status_txt.setHidden(False)
+        self.pytubeCallThread.start(self.yt_url.text())
+
+class pytubeCallThread(QThread):
+
+    def __init__(self):
+        QThread.__init__(self)
+
+    def __del__(self):
+        self.wait()
+
+    def run(self, url):
+        self.yt = pytube.YouTube(url)
         print('Video')
         self.videos = self.yt.streams.all()
         for x in self.videos:
             print(x)
         print('Audio')
-        #to get only audio use
+        #get only audio 
         self.audio = self.yt.streams.filter(only_audio=True).all()
         for x in self.audio:
             print(x) 
+        self.status_txt.setHidden(True)
+
 
 app = QApplication(sys.argv)
 main = MyWindow()
