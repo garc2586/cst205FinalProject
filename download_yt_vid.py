819b2b515898deb2d7327eb88df550d335d2c3a4
garc2586 <31326232+garc2586@users.noreply.github.com>
2018-12-03 11:25:19 -0800

    added video download function

--- download_yt_vid.py
+++ download_yt_vid.py
@@ -3,7 +3,9 @@ import pytube
 link = "https://www.youtube.com/watch?v=PTpgUmIWPis"
 yt = pytube.YouTube(link)
 print('Video')
-videos = yt.streams.all()
+all_videos = yt.streams.all()
+
+videos = yt.streams.filter(only_video=True).all()
 for x in videos:
 	print(x)
 
@@ -12,8 +14,8 @@ print('Audio')
 audio = yt.streams.filter(only_audio=True).all()
 for x in audio:
 	print(x)
-audio_download = yt.streams.filter(only_audio=True).first()
-audio_download.download('C:\\Users\\cgarc\\Desktop\\cst205\\final_project\\yt_vids')
+#audio_download = yt.streams.filter(only_audio=True).first()
+#audio_download.download('C:\\Users\\cgarc\\Desktop\\cst205\\final_project\\yt_vids')
 
 #stream = yt.streams.first()
 #stream.download('C:\\Users\\cgarc\\Desktop\\cst205\\final_project\\yt_vids')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, pytube
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton,
                                 QLineEdit, QHBoxLayout, QVBoxLayout, QComboBox)
 from PyQt5.QtCore import pyqtSlot
@@ -6,17 +6,21 @@ from PyQt5.QtCore import pyqtSlot
 
 class MyWindow(QWidget):
     def __init__(self):
+
         super().__init__()
 
         hbox1 = QHBoxLayout()
         self.label1 = QLabel('Enter url: ')
-        self.line_edit = QLineEdit()
+        self.yt_url = QLineEdit()
 
         hbox1.addWidget(self.label1)
-        hbox1.addWidget(self.line_edit)
+        hbox1.addWidget(self.yt_url)
 
         self.button = QPushButton('Search', self)
         hbox1.addWidget(self.button)
+
+        #call function to get video info
+        self.button.clicked.connect(self.getVideo)
 
         self.setLayout(hbox1)
         self.title = 'Audio/Video Downloader'
@@ -30,6 +34,19 @@ class MyWindow(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.show()
+
+    def getVideo(self):
+        self.link = "https://www.youtube.com/watch?v=PTpgUmIWPis"
+        self.yt = pytube.YouTube(self.link)
+        print('Video')
+        self.videos = self.yt.streams.all()
+        for x in self.videos:
+            print(x)
+        print('Audio')
+        #to get only audio use
+        self.audio = self.yt.streams.filter(only_audio=True).all()
+        for x in self.audio:
+            print(x) 
 
 app = QApplication(sys.argv)
 main = MyWindow()
